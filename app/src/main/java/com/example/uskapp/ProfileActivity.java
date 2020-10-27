ae0daf51397bb3f3b4f5b3db48dc8d718480fa22
Fattyboy9898 <justin_peng@mymail.sutd.edu.sg>
2020-10-27 23:47:03 +0800

    added nav bar and fixed cardview //still need fix profile picture not appear at first

--- app/src/main/java/com/example/uskapp/ProfileActivity.java
+++ app/src/main/java/com/example/uskapp/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.uskapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.drawable.RoundedBitmapDrawable;
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
@@ -7,15 +8,19 @@ import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
     User myUser;
     ImageView profilePicIV;
     TextView nameView,rankView,karmaView;
     ProgressBar expBar;
+    BottomNavigationView bottomNavView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +33,7 @@ public class ProfileActivity extends AppCompatActivity {
         rankView=findViewById(R.id.rankTv);
         karmaView = findViewById(R.id.karmaTv);
         expBar = findViewById(R.id.expProgressBar);
+        bottomNavView = findViewById(R.id.bottomNavigationView);
 
         if(myUser.getProfilePic()==null){
             profilePicIV.setImageResource(R.drawable.ic_launcher_foreground);
@@ -46,5 +52,15 @@ public class ProfileActivity extends AppCompatActivity {
         expBar.setMax(100);
         expBar.setProgress(myUser.getExp());
         expBar.setProgress(50);
+
+
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        });
     }
+
+
 }
