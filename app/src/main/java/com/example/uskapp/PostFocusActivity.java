753a1c1bf36406700fa30620df5c5673d3f1d21c
arnoldd98 <arnold.limzd@gmail.com>
2020-11-02 17:26:53 +0800

    Added PostFocusActivity

--- app/src/main/java/com/example/uskapp/PostFocusActivity.java
+++ app/src/main/java/com/example/uskapp/PostFocusActivity.java
@@ -5,19 +5,46 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.view.ViewGroup;
 
 public class PostFocusActivity extends AppCompatActivity {
+    RecyclerView answer_recyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_focus);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        answer_recyclerview = findViewById(R.id.answer_recycler_view);
+        answer_recyclerview.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        LinearLayoutManager linear_layout_manager = new LinearLayoutManager(this);
+        linear_layout_manager.setStackFromEnd(false);
+        answer_recyclerview.setLayoutManager(linear_layout_manager);
+        RecyclerView.Adapter answer_adapter = new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        }
 
     }
 }
