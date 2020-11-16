8c9e7fcec23b703cbede3d297506dec9df122e46
Fattyboy9898 <justin_peng@mymail.sutd.edu.sg>
2020-11-16 23:21:16 +0800

    changed User, Post and added intefaces for Observer changes made so that we can use Firebase

--- app/src/main/java/com/example/uskapp/AnswerPost.java
+++ app/src/main/java/com/example/uskapp/AnswerPost.java
@@ -3,9 +3,27 @@ package com.example.uskapp;
 import java.util.ArrayList;
 
 public class AnswerPost extends Post {
-    public AnswerPost(User user, boolean toggle_anonymity, String text, ArrayList<byte[]> images) {
-        super(user, toggle_anonymity, text, images);
+    ArrayList<Observer> followersOfPost = new ArrayList<>();
+    public AnswerPost(String userID, String postID, String text, String timestamp, String subject,
+                      boolean toggle_anonymity) {
+        super(userID, postID,text, timestamp, subject, toggle_anonymity);
     }
 
 
+    @Override
+    public void register(Observer o) {
+        followersOfPost.add(o);
+    }
+
+    @Override
+    public void unregister(Observer o) {
+        followersOfPost.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o : followersOfPost){
+            o.update("updates blah blah");
+        }
+    }
 }
--- app/src/main/java/com/example/uskapp/Post.java
+++ app/src/main/java/com/example/uskapp/Post.java
@@ -4,28 +4,74 @@ import android.media.Image;
 
 import java.util.ArrayList;
 
-public abstract class Post {
-    private User user;
+public abstract class Post implements Subject{
+    private String userID;
+    private String postID;
+    private ArrayList<String> postImageIDs =new ArrayList<String>();
+    private ArrayList<String> answerPostIDs = new ArrayList<String>(); // array lists need be created IDS added later on thru method
     public boolean toggle_anonymity;
     private String text;
-    private ArrayList<byte[]> images;
     private int upvotes;
     private String timestamp;
+    private String subject; // subject of post eg. 50.001
 
-    public Post(User user, boolean toggle_anonymity, String text, ArrayList<byte[]> images) {
-        this.user = user;
+
+    public Post(String userID, String postID, String text, String timestamp, String subject,
+                boolean toggle_anonymity) {
+        this.userID = userID;
+        this.postID=postID;
         this.toggle_anonymity = toggle_anonymity;
         this.text = text;
-        this.images = images;
+        this.upvotes = 0;
+        this.timestamp = timestamp;
+        this.subject = subject;
     }
 
-    public ArrayList<byte[]> getImages() {
-        return images;
+    public String getUserID() {
+        return userID;
     }
 
-    public User getUser() {
-        return user;
+    public String getPostID() {
+        return postID;
     }
+
+    public ArrayList<String> getPostImageIDs() {
+        return postImageIDs;
+    }
+
+    public void setPostImageIDs(ArrayList<String> postImageIDs) {
+        this.postImageIDs = postImageIDs;
+    }
+
+    public ArrayList<String> getAnswerPostIDs() {
+        return answerPostIDs;
+    }
+
+    public void setAnswerPostIDs(ArrayList<String> answerPostIDs) {
+        this.answerPostIDs = answerPostIDs;
+    }
+
+    public boolean isToggle_anonymity() {
+        return toggle_anonymity;
+    }
+
+    public void setToggle_anonymity(boolean toggle_anonymity) {
+        this.toggle_anonymity = toggle_anonymity;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setUpvotes(int upvotes) {
+        this.upvotes = upvotes;
+    }
+
+
+    public String getSubject() {
+        return subject;
+    }
+
 
     public String getText() {
         return text;
