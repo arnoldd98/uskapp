d47652b7ad3da829e40fbeeea987d3cea5eb62fe
USER <xfrixtando@gmail.com>
2020-11-18 15:22:53 +0800

    fixed camera thingies magic

--- app/src/main/java/com/example/uskapp/NewPostActivity.java
+++ app/src/main/java/com/example/uskapp/NewPostActivity.java
@@ -1,18 +1,26 @@
 package com.example.uskapp;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
 import java.io.IOException;
 
 public class NewPostActivity extends AppCompatActivity {
@@ -52,6 +60,7 @@ public class NewPostActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 buttonPostAs.setText("Clicked");
+                //anonymousOptions();
                 //Snackbar.make(findViewById(R.id.AnonymousOrNormal),Snackbar.LENGTH_SHORT).show();
             }
         });
@@ -103,6 +112,31 @@ public class NewPostActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static void anonymousOptions(final Activity activity, final PackageManager packageManager) {
+        final Dialog bottomDialogue = new Dialog(activity, R.style.ImageDialogSheet);
+        bottomDialogue.setContentView(R.layout.choose_anonymous_options_view);
+
+        WindowManager.LayoutParams dialog_params = new WindowManager.LayoutParams();
+        dialog_params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        dialog_params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog_params.gravity = Gravity.BOTTOM;
+        dialog_params.windowAnimations = R.style.SlideDialogAnimation;
+        bottomDialogue.getWindow().setAttributes(dialog_params);
+        bottomDialogue.getWindow().setBackgroundDrawableResource(R.drawable.holo_gray_btn);
+
+        bottomDialogue.setCanceledOnTouchOutside(true);
+        bottomDialogue.setCancelable(true);
+        bottomDialogue.show();
+
+        LinearLayout camera_option_layout = (LinearLayout) bottomDialogue.findViewById(R.id.open_camera_selectable_layout);
+        camera_option_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Robloks");
+            }
+        });
     }
 
 }
