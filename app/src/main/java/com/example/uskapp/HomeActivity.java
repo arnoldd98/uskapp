9216fee3ad2be73cfeb939772ebd448a39e52538
Fattyboy9898 <justin_peng@mymail.sutd.edu.sg>
2020-11-20 16:16:41 +0800

    able to upvote and not overvote

--- app/src/main/java/com/example/uskapp/HomeActivity.java
+++ app/src/main/java/com/example/uskapp/HomeActivity.java
@@ -80,14 +80,21 @@ public class HomeActivity extends BaseNavigationActivity {
                         String timestamp = s.child("timestamp").getValue(String.class);
                         boolean toggle_anonymity = s.child("toggle_anonymity").getValue(Boolean.class);
                         String subject = s.child("subject").getValue(String.class);
-                        DataSnapshot arraySnap = s.child("answerPostIDs");
+                        int upvotes  = s.child("upvotes").getValue(Integer.class);
+                        DataSnapshot arraySnapAnsID = s.child("answerPostIDs");
+                        DataSnapshot arraySnapVoteID = s.child("usersWhoUpVoted");
 
-                        QuestionPost qnPost = new QuestionPost(name,userID,postID,text,timestamp,subject,toggle_anonymity);
+                        QuestionPost qnPost = new QuestionPost(name,userID,postID,text,timestamp,subject,toggle_anonymity,upvotes);
                         posts_list.add(qnPost);
                         int i = 0;
-                        for(DataSnapshot id : arraySnap.getChildren()){
+                        for(DataSnapshot id : arraySnapAnsID.getChildren()){
                             String value = id.getValue(String.class);
                             qnPost.addAnswerPostID(value);
+                        }
+                        int j=0;
+                        for(DataSnapshot id : arraySnapVoteID.getChildren()){
+                            String value = id.getValue(String.class);
+                            qnPost.addUserUpvote(value);
                         }
                         StorageReference imageRef = FirebaseStorage.getInstance().getReference("ProfilePictures")
                                 .child(userID);
